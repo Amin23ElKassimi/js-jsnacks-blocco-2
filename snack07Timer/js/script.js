b5fad82843eadb9673bdeaf34e05ed4f0e0bd4c9
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-12-02 17:14:03 +0100

    Timer Finito

--- snack07Timer/js/script.js
+++ snack07Timer/js/script.js
@@ -6,5 +6,30 @@
 //  bisogna cliccare su un secondo pulsante
 
 
-const cronometro = document.getElementById('cronometro');
+
+
+
+let actualTime = 0;
+const seconds = document.querySelector('#seconds');
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+let clock = 0;
+
+start.addEventListener("click", function(){
+
+     clock = setInterval(function() {
+        actualTime = actualTime + 1;
+        console.log(actualTime);
+        seconds.innerHTML = actualTime; 
+    }, 1000);
+    
+      
+
+});
+
+stop.addEventListener("click", function(){
+
+    clearInterval(clock);
+
+});
 
